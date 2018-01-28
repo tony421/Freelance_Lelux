7cdf36df09c56454cf5ce038e4fc8a2a3eca5fde
tony <thawatchai.j14@gmail.com>
2018-01-29 00:15:20 +1030

    - uncaching main-id.css in script-main.php

--- master-page/script-main.php
+++ master-page/script-main.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="../bootstrap-3.3.6/css/bootstrap.min.css">
 <script type="text/javascript" src="../bootstrap-3.3.6/js/bootstrap.min.js"></script>
     
-<link rel="stylesheet" href="../css/main-id.css">
+<link rel="stylesheet" href="../css/main-id.css?<?php echo time(); ?>">
 <link rel="stylesheet" href="../css/main-class.css?<?php echo time(); ?>">
 <link rel="stylesheet" href="../css/messagebox.css">
 <link rel="stylesheet" href="../css/loadingpanel.css">
