f3c6d465622c5f1f11540b26d89a32ce230ca228
tony421 <thawatchai.j14@gmail.com>
2018-11-09 11:31:04 +1100

    Update reception_config.php

--- config/reception_config.php
+++ config/reception_config.php
@@ -4,8 +4,8 @@ define('RECEPTION_HOUR_RATE', '10');
 define('RECEPTION_LATE_NIGHT_RATE', '5');
 
 // Work on PHP 5.6 and newer
-const RECEPTION_COM_SALES = array(2249, 2499, 2699, 3499, 4199, 99999999);
-const RECEPTION_COM_RATES = array(0, 10, 20, 30, 40, 50);
+const RECEPTION_COM_SALES = array(2249, 2499, 2699, 3499, 4199, 4499, 4999, 99999999);
+const RECEPTION_COM_RATES = array(0, 10, 20, 30, 40, 50, 60, 70);
 
 // Work only at PHP 7
 //define('RECEPTION_COM_SALES', array(2249, 2499, 2699, 3499, 99999999));
